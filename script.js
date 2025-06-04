7148786c329afe608d16e3fc667f5ae960b30d21
chen-k-06 <kkc2160@columbia.edu>
2025-06-04 00:58:03 -0400

    Backspace debugging

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ document.addEventListener('keydown', function (event) {
     else if (key === 'Backspace') {
         event.preventDefault();
 
-        updateTile(currentRow, currentGuess.length, ' ');
+        updateTile(currentRow, currentGuess.length, '');
         currentGuess = currentGuess.slice(0, -1);
         console.log('Deleted. Current guess:', currentGuess);
     }
