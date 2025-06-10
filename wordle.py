fb8aa1b6b1f519b3d8d160dc4b92afdfc824312c
Kexin <kaitlynkexinchen@gmail.com>
2025-06-10 16:22:42 +0100

    Full entropy implementation, not accounting for word freqiuences

--- wordle.py
+++ wordle.py
@@ -54,25 +54,51 @@ def get_feedback(guess: str, secret_word: str) -> str:
         if letter != "-" and output_upper.count(letter) > secret_word.count(letter) and output[i] != output[i].upper(): 
             output[i] = "-"
 
-    return(output)
+    return(''.join(output))
 
-def uniform_entropy(outcomes):
+def get_uniform_entropy(outcomes):
     N = len(outcomes)
     if N == 0:
         return 0
     return math.log2(N)
 
+# returns an integer, the entropy of that event given a list of outcomes & probabilities
+def get_entropy(probabilities):
+    entropy = 0.0
+    if not probabilities: 
+        return 0
+    
+    for probability in probabilities:
+        if (probability > 0):
+            entropy += -1*probability*math.log2(probability)
+    return entropy
+    
+
 def rank_guesses(possible_guesses, possible_answers):
-    possible_answers_copy = [i for i in possible_answers]
-    for answer in possible_answers:
-        feedbacks = []
-        for guess in possible_guesses:
+    entropies = {}
+    feedback_cache = {}
+
+    for guess in possible_guesses:
+        for answer in possible_answers:
+                feedback_cache[(guess, answer)] = get_feedback(guess, answer)
+
+    for guess in possible_guesses:
+        feedback_counts = {}            
+        for answer in possible_answers:
             #For each possible answer in your current list, compute the feedback pattern you would get if you guessed this word.
-            feedbacks.append(get_feedback(guess, answer))
+            feedback = feedback_cache[(guess, answer)]
+            feedback_counts[feedback] = feedback_counts.get(feedback, 0) + 1
+        
+        probabilities = [count / len(possible_answers) for count in feedback_counts.values()]
+        entropy = get_entropy(probabilities)
+        entropies[guess] = entropy
+
+    sorted_entropies = {key: value for key, value in sorted(entropies.items(), key=lambda item: item[1], reverse=True)}    
+    print(sorted_entropies)
+    return sorted_entropies
 
 
-
-def get_AI_guess(guesses: list[str], feedback: list[str], secret_words: set[str], valid_guesses: set[str], guess_number: int) -> str:
+def get_AI_guess(guesses: list[str], feedback: list[str], secret_words: set[str], valid_guesses: set[str], guess_number: int) -> tuple[float, str]:
     '''Analyzes feedback from previous guesses/feedback (if any) to make a new guess
         
         Args:
@@ -87,26 +113,33 @@ def get_AI_guess(guesses: list[str], feedback: list[str], secret_words: set[str]
     '''
     #first guess should always be slate, mathmatically proven best starting word
     if (guesses[0] == ""): 
-        return "SLATE"
+        return (13.66,"SLATE")
     
-    secret_words_copy = [i for i in secret_words]
-    last_guess = guesses[guess_number - 1]
+    valid_guesses_copy = []
     
-    #checks which words share the same feedback result as the guess
-    secret_words_copy = [
-        word for word in secret_words_copy
-        if get_feedback(last_guess, word) == feedback
-    ]
+    # checks which words share the same feedback result as the guess
+    # end game code 
+    for guess in valid_guesses: 
+        flag = False
+        for i in range(len(feedback)): 
+            item = feedback[i]
+            last_guess = guesses[i]
+            if get_feedback(last_guess, guess) != item:
+                flag = True
+                break
+        if flag == False: 
+            valid_guesses_copy.append(guess)
     
-    bits_remaining = uniform_entropy(secret_words_copy)
-    print("potential words left: ", len(secret_words_copy))
+    bits_remaining = get_uniform_entropy(valid_guesses_copy)
+    print("potential words left: ", len(valid_guesses_copy))
     print("bits left: ", bits_remaining)
 
-    while next_guess in guesses: 
-        next_guess = random.choice(list(secret_words))
+    # valid_guesses = valid_guesses.remove(next_guess)
+    if (bits_remaining == 0) :
+        return (0, valid_guesses_copy[0])
 
-    valid_guesses = valid_guesses.remove(next_guess)
-    return (next_guess.upper())
+    guesses = rank_guesses(valid_guesses, valid_guesses_copy)
+    return (bits_remaining, list(guesses)[0])
 
 
 #selects a secret word at random from the official Wordle list
@@ -116,32 +149,31 @@ def get_secret_word():
     return secret_word
 
 #plays the Wordle game
+# will need to be rewritten in JS
 def wordle_game(secret_word: str):
     secret_words = list(get_secret_words())
     valid_guesses = list(get_valid_wordle_guesses())
     guesses = ["", "", "", "", "", ""]
-
+    feedbacks = []
+    bits = [] # uncertainty left in the answer space. should be strictly decreasing
     #user input guesses
     for i in range(6):
-        guess = ""
+        guess = "HINT"
 
         while (guess.upper() not in valid_guesses): 
-            guess = input("Enter guess, or 'hint' for a hint: ")
+            # guess = input("Enter guess, or 'hint' for a hint: ")
 
             #AI guess input
             if (guess.upper() == "HINT"):
-                if (i == 0):
-                    guess = get_AI_guess(guesses, [], secret_words, valid_guesses, 0)
-                    break
-
-                else:
-                    guess = get_AI_guess(guesses, get_feedback(guesses[i-1], secret_word), secret_words, valid_guesses, i)
+                    bits_remaining, guess = get_AI_guess(guesses, feedbacks, secret_words, valid_guesses, i)
+                    bits.append(bits_remaining)
                     break
 
             elif (guess.upper() not in valid_guesses): 
                 print("Not a valid guess. Please try again.")
-
+             
         guesses[i] = guess
+        feedbacks.append(get_feedback(guess, secret_word))
 
         #output formatting
         print(Back.LIGHTBLACK_EX + '       ') 
