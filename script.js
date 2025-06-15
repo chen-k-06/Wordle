a90735f0b7d6be8df33dd543cde3cb99675432e0
Kexin <kkc2160@columbia.edu>
2025-06-15 21:36:16 +0100

    Imported msgpack universally

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function updateTileBackspace(row, column, letter) {
 }
 
 // API functions 
-import { decode } from "https://cdn.jsdelivr.net/npm/@msgpack/msgpack@2.7.1/dist.es2019/esm/msgpack.min.js";
+// import { decode } from "https://cdn.jsdelivr.net/npm/@msgpack/msgpack@2.7.1/dist.es2019/esm/msgpack.min.js";
 async function loadFeedbackDict() {
     const response = await fetch('https://raw.githubusercontent.com/chen-k-06/Wordle/main/pattern_cache.msgpack');
     const arrayBuffer = await response.arrayBuffer();
