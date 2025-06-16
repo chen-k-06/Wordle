aa0d4cc280ab83317de196c61e08a40dea643cea
Kexin <kkc2160@columbia.edu>
2025-06-16 18:02:35 +0100

    Undo previous

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://chen-k-06.github.io/Wordle/"],  # frontend URL
+    allow_origins=["https://chen-k-06.github.io"],  # frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
