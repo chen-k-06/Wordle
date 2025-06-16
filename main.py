e0c09448d9582eb506b97f36bc3ff6f136e9f8ad
Kexin <kkc2160@columbia.edu>
2025-06-16 15:30:13 +0100

    Typo

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
     # allow_origins=["https://chen-k-06.github.io"],  # frontend URL
     allow_credentials=True,
     allow_methods=["*"],
