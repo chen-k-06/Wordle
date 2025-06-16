5ab44ee42503f4365d9ec70a05fe2650b260a924
Kexin <kkc2160@columbia.edu>
2025-06-16 18:11:45 +0100

    Debugging

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow all origins temporarily
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
